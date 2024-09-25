6295a8d2c07006fb7bb7baab1220da40b9759b13
Roman <romanoreshchenko@gmail.com>
2024-09-25 16:21:14 +0200

    Added converter Kilometers to Miles

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,20 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Это приложение для конвертирования миль в километры и километры в мили");
-        double miles = 10;
+        {double miles = 10;
         double km = Converter_M_to_KM(miles);
-        System.out.println("Перевод " + miles + " миль в километры. Результат: " + km + " километров." );
+        System.out.println("Перевод " + miles + " миль в километры. Результат: " + km + " километров." );}
+        double km = 16.0934;
+        double miles = Converter_KM_to_M(km);
+        System.out.println("Перевод " + km + " километров в мили. Результат: " + miles + " миль." );
     }
 
     private static double Converter_M_to_KM(double miles){
         return miles * CONST_M_TO_KM;
+    }
+
+    private static double Converter_KM_to_M(double kilometers){
+        return kilometers/CONST_M_TO_KM;
     }
 
 }
