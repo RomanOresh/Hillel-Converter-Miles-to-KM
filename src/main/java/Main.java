6f2c282031f8f7ec5a53c8225f702b6098618aff
Roman <romanoreshchenko@gmail.com>
2024-09-25 16:08:41 +0200

    Added app description

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+    private static final double CONST_M_TO_KM = 1.60934;
 
+    public static void main(String[] args) {
+        System.out.println("Это приложение для конвертирования миль в километры и километры в мили");
+    }
 
+    private static void CONVERTER_M_TO_KM(double Miles){
 
+    }
 }
